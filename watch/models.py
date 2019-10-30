30cbbbe30fa3e2a3e506d75d727deed8359fc260
moharick <moharick@gmail.com>
2019-10-30 17:42:42 +0300

    This commit creates the third model class.

--- watch/models.py
+++ watch/models.py
@@ -44,3 +44,27 @@ class UserProfile(models.Model):
     def __str__(self):
         return f'{self.user.username}'
 
+class Biz(models.Model):
+    name = models.CharField(max_length=30)
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    biz_location = models.CharField(max_length=30,blank=True)
+    biz_mtaa = models.ForeignKey('Mtaa',on_delete=models.CASCADE)
+    email = models.EmailField()
+
+    def create_biz(self):
+        self.save()
+
+    def delete_biz(self):
+        self.delete()
+
+    @classmethod
+    def find_biz(cls,biz_id):
+        biz = cls.objects.get(id=biz_id)
+        return biz
+
+    def update_biz(self,name):
+        self.name = name
+        self.save()
+
+    def __str__(self):
+        return f'{self.name}'
