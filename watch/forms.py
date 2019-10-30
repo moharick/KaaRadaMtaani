dd5bd7bb2a580c1af271420615df3eaf43e6ef4f
moharick <moharick@gmail.com>
2019-10-30 17:56:29 +0300

    This commit creates other model forms.

--- watch/forms.py
+++ watch/forms.py
@@ -9,3 +9,18 @@ class MtaaForm(ModelForm):
     class Meta:
         model = Mtaa
         fields = ('mtaa_name',)
+
+class AddBizForm(ModelForm):
+    class Meta:
+        model = Biz
+        fields = ('name','biz_location','email')
+
+class UpdateProfileForm(ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ('first_name','last_name','mtaa_name',)
+
+class PostForm(ModelForm):
+    class Meta:
+        model = Post
+        fields = ('title','post_description',)
