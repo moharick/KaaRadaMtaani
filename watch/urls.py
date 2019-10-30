df5131a0f41d8ed64227b42a992a40400a5e8af9
moharick <moharick@gmail.com>
2019-10-30 18:09:03 +0300

    This commit updates the url pattern.

--- watch/urls.py
+++ watch/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 urlpatterns=[
-     url(r'^$',views.index, name='index'),
+     url(r'^$',views.home, name='home'),
      ]
 if settings.DEBUG:
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
