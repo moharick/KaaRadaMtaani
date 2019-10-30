37c6363fe30e00a62a5a87026428ef8cf3342109
moharick <moharick@gmail.com>
2019-10-30 17:40:19 +0300

    This commit creates the second model class.

--- watch/models.py
+++ watch/models.py
@@ -23,3 +23,24 @@ class Mtaa(models.Model):
 
     def __str__(self):
         return f'{self.mtaa_name}'
+
+class UserProfile(models.Model):
+    first_name = models.CharField(max_length=20,blank=True)
+    last_name = models.CharField(max_length=20,blank=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    location = models.CharField(max_length=30,blank=True)
+    mtaa_name = models.ForeignKey('Mtaa',on_delete=models.CASCADE,null=True,blank=True)
+
+    def assign_mtaa(self,mtaa):
+        self.mtaa= mtaa
+        self.save()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def __str__(self):
+        return f'{self.user.username}'
+
