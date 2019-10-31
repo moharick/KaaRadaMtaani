70969b7f851500b50186348bc6045e121d383791
moharick <moharick@gmail.com>
2019-10-31 10:22:28 +0300

    This commit registers models in the admin.

--- watch/admin.py
+++ watch/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+admin.site.register(UserProfile)
+admin.site.register(Biz)
+admin.site.register(Post)
+admin.site.register(Mtaa)
