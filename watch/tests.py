c65b9dbc351c16851a0406381701ad8cd3ab4f74
moharick <moharick@gmail.com>
2019-10-31 12:17:27 +0300

    This commit creates tests.

--- watch/tests.py
+++ watch/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
-
+from .models import *
+from django.contrib.auth.models import User
 # Create your tests here.
+
+class UserProfileTestClass(TestCase):
+    def setUp(self):
+        self.new_user = User.objects.create_user(username='user',password='user-password')
+        self.new_profile = UserProfile(id=1,user=self.new_user,profile_pic='photos/pic',bio='user-bio')
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_profile,UserProfile))
+
+    def test_save_profile(self):
+        self.new_profile.save_profile()
+        profiles = UserProfile.objects.all()
+        self.assertTrue(len(profiles) > 0)
+
+    def test_delete_profile(self):
+        self.new_profile.delete_profile()
+        profiles = UserProfile.objects.all()
+        self.assertTrue(len(profiles) == 0)
+
+
+
+
