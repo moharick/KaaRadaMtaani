fa0497e65ad9988051a1e867e8ae0e3239c3ac16
moharick <moharick@gmail.com>
2019-10-31 08:47:42 +0300

    This commit creates url patterns for the views.

--- watch/urls.py
+++ watch/urls.py
@@ -3,8 +3,12 @@ from django.conf.urls import url
 from django.conf.urls.static import static
 from django.conf import settings
 
-urlpatterns=[
-     url(r'^$',views.home, name='home'),
-     ]
+urlpatterns = [
+    url(r'^$', views.home, name='home'),
+    url(r'^mtaa/(\d+)', views.mtaa, name='mtaa'),
+    url(r'^profile/(\d+)', views.profile, name='profile'),
+    url(r'^add_biz/', views.add_biz, name='add_biz'),
+    url(r'^search/', views.search, name='search'),
+]
 if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
