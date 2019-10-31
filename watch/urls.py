567f4085ff9b7dce0a80c6e153d9b50a904effc2
moharick <moharick@gmail.com>
2019-10-31 12:06:03 +0300

    This commit adds url patterns.

--- watch/urls.py
+++ watch/urls.py
@@ -2,9 +2,11 @@ from . import views
 from django.conf.urls import url
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^accounts/login/$', auth_views.LoginView.as_view(template_name="registration/login.html"), name="login"),
     url(r'^mtaa/(\d+)', views.mtaa, name='mtaa'),
     url(r'^profile/(\d+)', views.profile, name='profile'),
     url(r'^add_biz/', views.add_biz, name='add_biz'),
